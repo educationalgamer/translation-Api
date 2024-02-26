77bf45ba9088d6e0ae63937d46ec7365ff74442e
educationalgamer <educationalgamerindia@gmail.com>
2024-02-26 20:40:33 +0530

    file added

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const translator = require('translator-for-you');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/translate', async (req, res) => {
+    // Check if request body contains 'text' key
+    try{
+        
+        const { text } = req.body;
+        if(!text){
+            return res.status(400).json({ error: 'Invalid request body'});
+        }
+      
+            // Translates the text into French
+            
+            const translation = await translator(text, 'fr');
+            if(!translation){
+                return res.status(400).json({error:'Something bad happened'})
+            }
+            res.status(200).json({ translation });
+    }catch(e){
+        return res.status(400).json({ error: 'Invalid request body. Please provide JSON' });
+    }
+    
+   
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
